bf498ff88cbe4930f2f000cbdf6d6c40551d28b6
AnKit <nkits9@gmail.com>
2020-05-11 22:10:01 +0530

    Soltions

--- c++/Count-pairs-with-given-sum.cpp
+++ c++/Count-pairs-with-given-sum.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int count = 0;
-int noOfPairs(int a[], int n, int k, int start, int numOfElements) {
+ 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+
+int noOfPairs(int a[], int n, int k, int start, int numOfElements, int &count) {
     if(k == 0 && numOfElements == 2) {
         count++;
         // printf("i = %d\n", start);
@@ -15,13 +18,14 @@ int noOfPairs(int a[], int n, int k, int start, int numOfElements) {
     }
     for(int i=start; i<n; i++) {
         k = k - a[i];
-        noOfPairs(a, n, k, i+1, numOfElements+1);
+        noOfPairs(a, n, k, i+1, numOfElements+1, count);
         k = k + a[i];
     }
     return count;
 }
 
 int main() {
+    IOS;
     int t; 
     cin >> t;
     while(t-- > 0) {
@@ -31,7 +35,7 @@ int main() {
         for(int i=0; i<n; i++) {
             cin >> a[i];
         }
-        count = 0;
-        cout << noOfPairs(a, n, k, 0, 0) << endl;
+        int count = 0;
+        cout << noOfPairs(a, n, k, 0, 0, count) << endl;
     }
 }
