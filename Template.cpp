7721325240f19aa48306e4a7a720c5023a575370
AnKit <nkits9@gmail.com>
2020-05-15 21:46:48 +0530

    Template for Competitive Programming

--- Template.cpp
+++ Template.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
+typedef pair<int, int>	pii;
+typedef vector<int>	vi;
+typedef vector<vi>	vvi;
 
 int main() {
 	IOS;
