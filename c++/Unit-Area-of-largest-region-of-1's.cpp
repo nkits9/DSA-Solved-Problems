bf1084b169166c0f35471ca2e330c8485f785287
AnKit <nkits9@gmail.com>
2020-05-17 09:07:41 +0530

    Solutions

--- c++/Unit-Area-of-largest-region-of-1's.cpp
+++ c++/Unit-Area-of-largest-region-of-1's.cpp
@@ -5,16 +5,16 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 
-void DFSUtil(int x, int y, int &n, int &m, vvi &mat, vvi &vis, int &count) {
-	vis[x][y] = 1; // Mark visited
+void DFSUtil(int x, int y, int &n, int &m, vvi &mat, int &count) {
+	mat[x][y] = 0; // Mark visited
 	int row[] = {0, 1, 1, 1, 0, -1, -1 , -1};
 	int col[] = {1, 1, 0, -1, -1, -1, 0, 1};
 	for (int k = 0; k < 8; k++) {
 		int c = x + row[k];
 		int d = y + col[k];
-		if (c >= 0 && c < n && d >= 0 && d < m && !vis[c][d] && mat[c][d]) {
+		if (c >= 0 && c < n && d >= 0 && d < m && mat[c][d]) {
 			count++;
-			DFSUtil(c, d, n, m, mat, vis, count);
+			DFSUtil(c, d, n, m, mat, count);
 		}
 	}
 	return;
@@ -27,19 +27,18 @@ int main() {
 	while (t--) {
 		cin >> n >> m;
 		vvi mat(n, vi(m));
-		vvi vis(n, vi(m, 0));
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				cin >> mat[i][j];
 			}
 		}
-		int maxRegion = 0, count = 1;
+		int maxRegion = 0, count;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (!vis[i][j] && mat[i][j]) {
-					DFSUtil(i, j, n, m, mat, vis, count);
-					if (count > maxRegion)   maxRegion = count;
+				if (mat[i][j]) {
 					count = 1;
+					DFSUtil(i, j, n, m, mat, count);
+					if (count > maxRegion)   maxRegion = count;
 				}
 			}
 		}
