4c6a60c080087627ce94ef51deebd42a984f6bc1
AnKit <nkits9@gmail.com>
2020-07-25 14:15:28 +0530

    Template for competitive programming

--- Template.cpp
+++ Template.cpp
@@ -1,20 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-typedef pair<int, int>	pii;
-typedef vector<int>	vi;
-typedef vector<vi>	vvi;
+#include <unordered_map>
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+#define MOD 1000000007
+
+//To print decimal upto x places type %.xf with printf function.
+const long double pi=3.1415926535897932384626433832; 
+
+void c_p_c() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
 
 int main() {
-	IOS;
-	int t;
-	cin >> t;
-	while(t--) {	
-		int a, b;
-		cin >> a >> b;
-		int sum = a + b;
-		cout << sum << endl;
-	}	
-	return 0;
+    IOS;
+    c_p_c();
+    int t;  cin >> t;
+    while(t--) {
+        
+    }
 }
