c4170e8cc61d5340e9195f9d12fe6366886e11e3
AnKit <nkits9@gmail.com>
2020-10-07 19:21:52 +0530

    Template for Competitive Programming

--- Template.cpp
+++ Template.cpp
@@ -10,24 +10,26 @@ using namespace std;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-
+#define INF 0x7f7f7f7f7f7f7f7f
 #define MOD 1000000007
-
-//To print decimal upto x places type %.xf with printf function.
 const long double pi=3.1415926535897932384626433832; 
+
+//cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
 
 void c_p_c() {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 }
+
 
 int main() {
     IOS;
     c_p_c();
-    int t;  cin >> t;
-    while(t--) {
-        
+    // Code starts from here:
+    int t; cin >> t;
+    while (t--) {
+            
     }
 }
