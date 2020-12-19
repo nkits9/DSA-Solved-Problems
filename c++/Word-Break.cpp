8838667e1965eadd05d9292d1f630ef5a71dd45b
AnKit <nkits9@gmail.com>
2020-12-19 18:51:09 +0530

    Solutions

--- c++/Word-Break.cpp
+++ c++/Word-Break.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     
-    string split(int start, int end, string &s) {
-        string word = "";
-        for(int i=start; i<=end; i++) {
-            word.push_back(s[i]);
-        }
-        return word;
-    }
-    
-    bool checkWordBreak(int i, string &s, vector<string> &dict, vector<int> dp) {
-        if(i>=s.size()) {   
+    bool checkWordBreak(int start, string s, unordered_set<string> &dict, vector<int> &dp) {
+        if(start >= s.size()) {
             return true;
         }
-        
-        if(dp[i] != -1) {
-            return dp[i]==1 ? true : false;
+
+        if(dp[start] != -1) {
+            return dp[start];
         }
 
-        for(int j=i; j<s.size(); j++) {
-            string word = split(i, j, s);
-            for(int k=0; k<dict.size(); k++) {
-                if(word.compare(dict[k]) == 0) {
-                    dp[i] = checkWordBreak(j+1, s, dict, dp);
-                    if(dp[i]==1)    return true;
-                    
-                }
+        for(int i=1; start+i <= s.size(); i++) {
+            string word = s.substr(start, i);
+
+            if(dict.find(word) != dict.end()) {
+                dp[start+i] = checkWordBreak(start+i, s, dict, dp);
+                if(dp[start+i]==1)  return true;
             }
+
         }
 
         return false;
     }
-    
+
+
+
     bool wordBreak(string s, vector<string>& wordDict) {
-        if(s.size() == 0) {
-            return false;
-        } else {
-            vector<int> dp(s.size(), -1);
-            return checkWordBreak(0, s, wordDict, dp);
+        if(s.size() == 0)   return false;
+        
+        vector<int> dp(s.size()+1, -1);
+        unordered_set<string> dict;
+
+        for(int i=0; i<wordDict.size(); i++) {
+            dict.insert(wordDict[i]);
         }
+
+        return checkWordBreak(0, s, dict, dp);
     }
     
 };
