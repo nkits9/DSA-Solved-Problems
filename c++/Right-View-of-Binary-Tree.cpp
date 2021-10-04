ee1a805f53bbc846d3dae0d491e71ddc6894c71a
Ankit Sharma <ankitsharma@Ankits-MacBook-Pro.local>
2021-10-04 09:59:34 +0530

    Solutions

--- c++/Right-View-of-Binary-Tree.cpp
+++ c++/Right-View-of-Binary-Tree.cpp
@@ -22,3 +22,65 @@ class Solution
         return vec;
     }
 };
+
+
+-------------------------------------------------------------------------------------------------------------------------
+
+// Similar Problem:
+
+1. https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+
+
+// Method 1: 
+void leftViewUtil(struct Node *root, int level, int* max_level) {
+    if (root==NULL)  return;
+    
+    // If this is the first node of its level
+    if (*max_level < level) {
+        cout << root->data << " ";
+        *max_level = level;
+    }
+    
+    // Recur for left and right subtrees  
+    leftViewUtil(root->left, level+1, max_level);
+    leftViewUtil(root->right, level+1, max_level);
+}
+
+
+void leftView(struct Node *root) {
+    int max_level = 0;
+
+    leftViewUtil(root, 1, &max_level);
+}
+
+    
+// Method 2: First element of each level in level order traversal of tree is the result.
+    
+vector<int> leftView(Node *root) {
+    vector<int> res;
+    if(root == NULL)    return res;
+
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()) {
+       int nodeCount = q.size();
+       int count = 0;
+        while(nodeCount--) {
+            Node* temp = q.front();
+            q.pop();
+
+            if(count==0) {
+                res.push_back(temp->data);
+            }
+            count++;
+
+            if(temp->left!=NULL)
+                q.push(temp->left);
+            if(temp->right!=NULL)
+                q.push(temp->right);
+        }
+    }
+
+    return res;
+}
--- c++/Symmetric-Tree.cpp
+++ c++/Symmetric-Tree.cpp
@@ -13,3 +13,40 @@ public:
         return isMirror(root->left, root->right);
     }
 };
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Similar Problems:
+
+1. https://practice.geeksforgeeks.org/problems/mirror-tree/1
+
+void mirror(Node* root) {
+    if(root == NULL)    return;
+    
+    Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    
+    mirror(root->left);
+    mirror(root->right);
+}
+
+
+
+2. https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+
+bool isIdentical(Node *a, Node *b) {
+    if (a == NULL && b == NULL)  
+        return true;  
+  
+    if (a == NULL || b == NULL)  
+        return false;  
+         
+    return (  
+        a->data == b->data &&  
+        isIdentical(a->left, b->left) &&  
+        isIdentical(a->right, b->right)  
+    );  
+}
