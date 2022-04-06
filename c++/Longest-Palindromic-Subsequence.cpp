dd7b648d1fd50ea19aca460fe1d5cd44de37e401
Ankit Sharma <nkits9@gmail.com>
2022-04-06 23:46:08 +0530

    Update Longest-Palindromic-Subsequence.cpp

--- c++/Longest-Palindromic-Subsequence.cpp
+++ c++/Longest-Palindromic-Subsequence.cpp
@@ -48,3 +48,35 @@ int longestPalindromeSubseq(string s) {
     return dp[0][n-1];
 }
 
+// Method 2: Using LCS 
+
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        
+        for(int i=0; i<=n; i++) {
+            for(int j=0; j<=m; j++) {
+        
+                if(i==0 || j==0) 
+                    dp[i][j] = 0;
+                
+                else if(s1[i-1] == s2[j-1]) 
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                
+                else 
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        string s1 = s;
+        reverse(s.begin(), s.end());
+        string s2 = s;
+        return longestCommonSubsequence(s1, s2);
+    }
+};
+
