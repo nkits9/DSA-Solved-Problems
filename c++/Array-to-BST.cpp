b8cd96b454baf9bd7dd8587ab91e26ab9d11a54a
Ankit Sharma <ankit27.sharma@paytm.com>
2022-04-27 23:39:57 +0530

    Solutions

--- c++/Array-to-BST.cpp
+++ c++/Array-to-BST.cpp
@@ -1,29 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
+class Solution {
 
-
-
-void createBST(int arr[], int l, int r) {
-    if(l > r)   return;
+public:
+    void buildTree(vector<int>& nums, int start, int end, vector<int> & res) {
+        if(start > end)     return;
+        
+        int mid = (start + end)/2;
+        
+        res.push_back(nums[mid]);
+        
+        buildTree(nums, start, mid-1, res);
+        buildTree(nums, mid+1, end, res);
+    }
     
-    int mid = (r-l)/2 + l;
-    cout << arr[mid] << " ";
-
-    createBST(arr, l, mid-1);
-    createBST(arr, mid+1, r);
-}
-
-int main() {
-	IOS;
-	int t; cin >> t;
-	while(t--) {
-	    int n; cin >> n;
-	    int arr[n];
-	    for(int i=0; i<n; i++)  cin >> arr[i];
-	    
-        createBST(arr, 0, n-1);
-        cout << endl;
-	}
+    vector<int> sortedArrayToBST(vector<int>& nums) {
+        vector<int> res;
+        buildTree(nums, 0, nums.size()-1, res);
+        return res;
+    }
 }
