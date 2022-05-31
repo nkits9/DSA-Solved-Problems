72b376d774571c2833960ed0aa63a75308045a1b
Ankit Sharma <nkits9@gmail.com>
2022-05-31 23:59:10 +0530

    Update Combination-Sum.cpp

--- c++/Combination-Sum.cpp
+++ c++/Combination-Sum.cpp
@@ -1,54 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 
-void findCombination(int *arr, int start, int n, int sum, vvi &result, vi curr) {
-    if(sum < 0) {
-        return;
-    }
-    if(sum == 0) {
-        result.push_back(curr);
-        return;
-    }
-    for(int i=start; i<n; i++) {
-        if(i==start || arr[i]!=arr[i-1]) {
-            curr.push_back(arr[i]);
-            findCombination(arr, i, n, sum-arr[i], result, curr);
-            curr.pop_back();
+class Solution {
+  public:
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    
+    void findCombination(vector<int> &arr, int start, int n, int sum, vvi &result, vi curr) {
+        if(sum < 0) {
+            return;
         }
-    }
-    return;
-
-}
-
-int main() {
-    IOS;
-    int t; cin >> t;
-    while(t--) {
-        int n; cin >> n;
-        int arr[n];
-        for(int i=0; i<n; i++) 
-            cin >> arr[i];
-        sort(arr, arr+n);
-        int sum; cin >> sum;
-        vvi res; vi curr;
-        findCombination(arr, 0, n, sum, res, curr);
-        
-        if(res.empty()) {
-            cout << "Empty" << endl;
+        if(sum == 0) {
+            result.push_back(curr);
+            return;
         }
-        else {
-            for(int i=0; i<res.size(); i++) {
-                cout << "(";
-                for(int j=0; j<res[i].size()-1; j++) {
-                    cout << res[i][j] << " ";
-                }
-                cout << res[i][res[i].size()-1] << ")";
+        for(int i=start; i<n; i++) {
+            if(i==start || arr[i]!=arr[i-1]) {
+                curr.push_back(arr[i]);
+                findCombination(arr, i, n, sum-arr[i], result, curr);
+                curr.pop_back();
             }
-            cout << endl;
         }
+        return;
+    
     }
-}
+    
+    vector<vector<int>> combinationSum(vector<int> &arr, int target) {
+        
+        sort(arr.begin(), arr.end());
+        
+        vvi res; vi curr;
+        
+        findCombination(arr, 0, arr.size(), target, res, curr);
+        
+        return res;
+    }
+};
