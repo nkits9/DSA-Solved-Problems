7a919c0bbc8ed6aa6eef194222957c5b52235c56
Ankit Sharma <nkits9@gmail.com>
2022-06-02 00:59:49 +0530

    Update Maximum-Path-Sum-between-2-Leaf-Nodes.cpp

--- c++/Maximum-Path-Sum-between-2-Leaf-Nodes.cpp
+++ c++/Maximum-Path-Sum-between-2-Leaf-Nodes.cpp
@@ -1,24 +1,29 @@
-int maxPath(Node *root, int &maxSum) {
-    if(root == NULL)    return 0;
+class Solution {
+public:
 
-    int l = maxPath(root->left, maxSum);
-    int r = maxPath(root->right, maxSum);
-
-    if (root->left && root->right) {
-        maxSum = max(maxSum, root->data + l + r);
-        return root->data + max(l, r);
+    int maxPath(Node *root, int &maxSum) {
+        if(root == NULL)    return 0;
+        
+        int l = maxPath(root->left, maxSum);
+        int r = maxPath(root->right, maxSum);
+        
+        if (root->left && root->right) {
+            maxSum = max(maxSum, root->data + l + r);
+            return root->data + max(l, r);
+        }
+        
+        return root->left ? root->data + l : root->data + r;
+    } 
+    
+    int maxPathSum(Node* root) {
+        int maxSum = INT_MIN;
+        int h = maxPath(root, maxSum);
+        
+        if (root->left == NULL || root->right == NULL) {
+            maxSum = max(maxSum, h);
+        }
+        
+        return maxSum;
     }
 
-    return root->left ? root->data + l : root->data + r;
-} 
-
-int maxPathSum(Node* root) {
-    int maxSum = INT_MIN;
-    int h = maxPath(root, maxSum);
-
-    if (root->left == NULL || root->right == NULL) {
-        maxSum = max(maxSum, h);
-    }
-
-    return maxSum;
-}
+};
