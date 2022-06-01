98ac18af6d8aef20530e667ea67df3e58bdf3205
Ankit Sharma <nkits9@gmail.com>
2022-06-01 10:15:23 +0530

    Update Rotten-Oranges.cpp

--- c++/Rotten-Oranges.cpp
+++ c++/Rotten-Oranges.cpp
@@ -1,3 +1,71 @@
+// Approach 1: Keep count of the maxtime for all oranges, once all the freshOranges are finished return maxtime.          
+
+#define mp make_pair
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+class Solution {
+    public:
+    
+    struct Orange {
+        pii pt;
+        int time;
+    };
+    
+    int orangesRotting(vvi& grid) {
+        int n = grid.size(), m = grid[0].size();
+        
+        queue<Orange> q;
+        
+        int freshOranges = 0;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(grid[i][j] == 2)
+                    q.push({mp(i, j), 0});
+                if(grid[i][j] == 1)
+                    freshOranges++;
+            }
+        }
+        
+        int row[] = {1, 0, -1, 0};
+        int col[] = {0, -1, 0, 1};
+        
+        int ans = INT_MIN;
+        
+        while(!q.empty()) {
+            int a = q.front().pt.first;
+            int b = q.front().pt.second;
+            int curr_time = q.front().time;
+            q.pop();
+            
+            ans = max(ans, curr_time);
+            
+            for(int k=0; k<4; k++) {
+                int x = a + row[k];
+                int y = b + col[k];
+                
+                if(x>=0 && x<n && y>=0 && y<m && grid[x][y] == 1) {
+                    grid[x][y] = 2;
+                    q.push({mp(x, y), curr_time+1});
+                    freshOranges--;
+                }
+            }
+            
+        }
+        
+        // check if all the oranges are rotten in the end
+        if(freshOranges != 0)   return -1;
+        
+        
+        return ans;
+    }
+};
+
+
+// Approach 2: Do Level order traversal in graph and keep the count of levels, which is equal to total time taken to rot oranges.
+
 #include <bits/stdc++.h>
 using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -6,8 +74,7 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 
-// Approach 1: Do Level order traversal in graph and keep the count of levels,
-//             which is equal to total time taken to rot oranges.
+
 int main() {
     IOS;
     int t, n, m, x, y;
@@ -47,62 +114,5 @@ int main() {
         }
         timeTaken = freshOranges==0 ? (timeTaken-1) : -1;
         cout << timeTaken << endl;
-    }
-    
-}
-
-/*Approach 2: Keep count of the maxtime for all oranges, 
-                once all the freshOranges are finished return maxtime.  
-*/          
-
-struct Oranges {
-    pii pt;
-    int time;
-};
-
-int main() {
-    IOS;
-    int t; cin >> t;
-    while(t--) {
-        int n, m;
-        cin >> n >> m;
-        int mat[n][m];
-        int freshOranges = 0;
-        queue<Oranges> q;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                cin >> mat[i][j];
-                if(mat[i][j]==1)    freshOranges++;
-                if(mat[i][j]==2)    q.push({{i, j}, 0});
-            }
-        }
-        int timeTaken = 0;
-        int row[] = {1, 0, -1, 0};
-        int col[] = {0, -1, 0, 1};
-        while(!q.empty()) {
-            int a = q.front().pt.first;
-            int b = q.front().pt.second;
-            int time = q.front().time;
-            q.pop();
-      
-            if(freshOranges == 0) {
-                cout << timeTaken << endl;
-                break;
-            }
-          
-            for(int k=0; k<4; k++) {
-                int x = a + row[k];
-                int y = b + col[k];
-                if(x>=0 && x<n && y>=0 && y<m && mat[x][y]==1) {
-                    mat[x][y] = 2;
-                    timeTaken = max(timeTaken, time+1);
-                    q.push({{x, y}, time+1});
-                    freshOranges--;
-                }
-            }
-        }
-        if(freshOranges != 0) {
-            cout << -1 << endl;
-        }
     }
 }
