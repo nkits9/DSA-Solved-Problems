19c1e7e70e3f58be6eea1c36192e2eb6a8094662
Ankit Sharma <nkits9@gmail.com>
2022-06-03 09:29:05 +0530

    Update Longest-K-unique-characters-substring.cpp

--- c++/Longest-K-unique-characters-substring.cpp
+++ c++/Longest-K-unique-characters-substring.cpp
@@ -1,28 +1,23 @@
 class Solution{
   public:
     int longestKSubstr(string s, int k) {
-        
-        int map[26] = {0};
-        int i=0, j=0, ans=-1, unique = 0;
+        int count = 0, ans = -1, i = 0, j = 0;
+        unordered_map<char, int> mp;
         
         while(j < s.size()) {
+            if(mp[s[j]] == 0)   count++;
+            mp[s[j]]++;
             
-            if(map[s[j] - 'a'] == 0)    unique++;
-            map[s[j] - 'a'] ++;
-            
-            if(unique == k) {
-                ans = max(ans, j-i+1 );
-            }
-            
-            while(i<=j && unique > k) {
-                map[s[i] - 'a']--;
-                if(map[s[i] - 'a'] == 0)    unique--;
+            if(count == k)  ans = max(ans, j-i+1);
+
+            while(i <= j && count > k) {
+                mp[s[i]]--;
+                if(mp[s[i]] == 0)   count--;
                 i++;
             }
-            
+
             j++;
         }
-
         return ans;
     }
 };
