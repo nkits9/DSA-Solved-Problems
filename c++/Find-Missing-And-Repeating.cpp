2ebb22f152da60b9ae508e99b3cee80a307f06df
Ankit Sharma <nkits9@gmail.com>
2022-06-06 14:14:37 +0530

    Update Find-Missing-And-Repeating.cpp

--- c++/Find-Missing-And-Repeating.cpp
+++ c++/Find-Missing-And-Repeating.cpp
@@ -1,16 +1,22 @@
-class Solution{
-public:
-    int *findTwoElement(int *arr, int n) {
-        int *ans = new int[2], i;
-        for (i = 0; i < n; i++) {
-            if (arr[abs(arr[i]) - 1] > 0)
-                arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1];
-            else
-                ans[0] = abs(arr[i]);
-        }
-        for (i = 0; i < n; i++) {
-            if (arr[i] > 0) ans[1] = i + 1;
-        }
-        return ans;
+int *findTwoElement(int *arr, int n) {
+    int missing, repeating;
+
+    for(int i=0; i<n; i++) {
+        if(arr[abs(arr[i]) - 1] > 0) 
+            arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1];
+        else
+            repeating = abs(arr[i]); 
     }
-};
+
+    for(int i=0; i<n; i++) {
+        if(arr[i] > 0) {
+            missing = i+1;
+        }
+    }
+
+    int *ans = new int[2];
+    ans[0] = repeating;
+    ans[1] = missing;
+
+    return ans;
+}
