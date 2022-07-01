d5b2e9ea164ba0b18494d781d11f0f8e73ae1e06
Ankit Sharma <nkits9@gmail.com>
2022-07-01 12:47:09 +0530

    Update Minimum-Swaps-for-Bracket-Balancing.cpp

--- c++/Minimum-Swaps-for-Bracket-Balancing.cpp
+++ c++/Minimum-Swaps-for-Bracket-Balancing.cpp
@@ -1,47 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
+int minimumNumberOfSwaps(string s) {
+    int close = 0, open = 0, unbal = 0, swaps = 0;
 
-long swapCount(char s[]) {
-    vector<int> pos;
-    int n = strlen(s);
-    // Keep track of '['
-    for(int i=0; i<n; i++) {
-        if(s[i]=='[')   pos.push_back(i);
-    }
-    int p = 0; // To track position of next '[' in pos.
-    int unbal = 0, ans = 0;
-    for(int i=0; i<n; i++) {
-        if(s[i]=='[') {
-            unbal++;
-            p++;
-        } 
-        else if( s[i]==']') {
+    for(int i=0; i<s.size(); i++) {
+        if(s[i] == '[') {
+            open++;
             if(unbal > 0) {
-                unbal--;
-            } 
-            else if(unbal==0) {
-                ans += pos[p] - i;
-                swap(s[i], s[pos[p]]);
-                i++;
-                p++;
+                swaps += unbal;
+                // swap(s[i], s[i-unbal]);
             }
         }
-    }
-    return ans;
-}
-
-int main() {
-    IOS;
-    int t; cin >> t;
-    while(t--) {
-        int n; cin >> n;
-        char s[n];
-        for(int i=0; i<n; i++) {
-            cin >> s[i];
+        else {
+            close++;
         }
-     
-        cout << swapCount(s) << endl;
+        unbal = close - open;
     }
+
+    // cout << s << endl;     (In the end string will be always like this - "[][][][]")
+    return swaps;
 }
