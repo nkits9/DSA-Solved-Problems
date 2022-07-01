af7e775ee0b5425dde3fddc85838f63638664203
Ankit Sharma <nkits9@gmail.com>
2022-07-01 13:06:26 +0530

    Update Activity-Selection.cpp

--- c++/Activity-Selection.cpp
+++ c++/Activity-Selection.cpp
@@ -1,27 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-typedef pair<int, int> pii;
 
-int main() {
-	IOS;
-	int t; cin >> t;
-	while(t--) {
-	    int n; cin >> n;
-	    pii p[n];
-	    for(int i=0; i<n; i++) cin >> p[i].second; // Putting the starting time as second.
-	    for(int i=0; i<n; i++) cin >> p[i].first;  // Putting the finshing time as first.
-	    
-	    sort(p, p+n);   // Now if we sort, it will sort according to the finish time.
-	    
-	    int ans = 1, j = 0;
-	    for(int i=1; i<n; i++) {
-	        if(p[i].second >= p[j].first) {
-	            ans++;
-	            j = i;
-	        }
-	    }
-	    cout << ans << endl;
+int activitySelection(vector<int> start, vector<int> end, int n) {  
+	vector<pair<int, int>> intervals(n);
+
+	for(int i=0; i<n; i++) {
+	    intervals[i].second = start[i];     // Putting the starting time as second.
+	    intervals[i].first = end[i];        // Putting the finshing time as first.
 	}
+
+	sort(intervals.begin(), intervals.end());   	// Sort according to the finish time.
+
+	int activitiesPerformed = 0, prevEnd = -1;
+
+	for(int i=0; i<n; i++) {
+	    if(intervals[i].second > prevEnd) {
+		activitiesPerformed++;
+		prevEnd = intervals[i].first;
+	    }
+	}
+
+	return activitiesPerformed;
 }
