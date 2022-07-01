4f3d0e2a86cc31e4373615737f30224f6bee3f1f
Ankit Sharma <nkits9@gmail.com>
2022-07-01 20:34:59 +0530

    Update Minimize-the-heights.cpp

--- c++/Minimize-the-heights.cpp
+++ c++/Minimize-the-heights.cpp
@@ -1,43 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
+int getMinDiff(int arr[], int n, int k) {
+	sort(arr, arr + n);
+	int ans = arr[n-1] - arr[0]; // Maximum possible height difference
 
-int getMinDiff(int arr[], int n, int k) { 
-    if (n == 1) 
-       return 0; 
-   
-    sort(arr, arr+n);
-    int ans = arr[n-1]-arr[0]; 
-    
-    int small = arr[0]+k; 
-    int big = arr[n-1]-k; 
+	int tmin, tmax;
 
-    if (small > big) 
-       swap(small, big); 
+	int big = arr[n-1] - k;
+	int small = arr[0] + k;
 
-    for (int i=1; i<n-1; i++) { 
-        int sub = arr[i] - k; 
-        int add = arr[i] + k; 
-        if (sub >= small || add <= big) 
-            continue;  
-        if (big-sub <= add-small) 
-            small = sub; 
-        else
-            big = add; 
-    } 
+	for(int i=1; i<n; i++) {
+		if(arr[i] - k < 0)  continue;
 
-    return  min(ans, big - small); 
-} 
-int main() {
-	IOS;
-	int t, k, n;
-	cin >> t;
-	while(t--) {
-	    cin >> k >> n;
-	    int arr[n];
-	    for(int i=0; i<n; i++) cin >> arr[i];
-	    
-	    cout << getMinDiff(arr, n, k) << endl;
+		tmin = min(small, arr[i] - k); // Minimum element when we add k to whole array
+		tmax = max(big, arr[i-1] + k); // Maximum element when we subtract k from whole array
+		ans = min(ans, tmax - tmin); 
 	}
+
+	return ans;
 }
