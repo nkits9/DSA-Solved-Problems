c3d4f395e0f7f375aed2d7702db58a03a23efd0e
Ankit Sharma <nkits9@gmail.com>
2022-07-02 14:36:43 +0530

    Update Minimum-Platforms.cpp

--- c++/Minimum-Platforms.cpp
+++ c++/Minimum-Platforms.cpp
@@ -3,7 +3,7 @@ int findPlatform(int arr[], int dep[], int n) {
 	sort(dep, dep+n);
 
 	int i=0, j=0, platform=0, occupied=0;
-
+	
 	while(i<n && j<n) {
 		if(arr[i] <= dep[j]) {
 			if(platform <= occupied)
