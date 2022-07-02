0dc250f7162af97222e2e0cd4b7f15509b37cddb
Ankit Sharma <nkits9@gmail.com>
2022-07-02 15:06:03 +0530

    Update Minimum-Cost-of-ropes.cpp

--- c++/Minimum-Cost-of-ropes.cpp
+++ c++/Minimum-Cost-of-ropes.cpp
@@ -4,31 +4,31 @@ using namespace std;
 #define endl "\n"
 
 int main() {
-	IOS;
-	int t; cin >> t;
-	while(t--) {
-	    int n, x; 
-	    cin >> n;
+    IOS;
+    int t; cin >> t;
+    while(t--) {
+        int n, x; 
+        cin >> n;
 
-	    priority_queue<int, vector<int>, greater<int>> pq;
-	    for(int i=0; i<n; i++) {
-	        cin >> x;
-	        pq.push(x);
-	    }
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=0; i<n; i++) {
+            cin >> x;
+            pq.push(x);
+        }
         
-	    long long int a, b, cost = 0;
-	    while(pq.size() > 1) {
-	        a = pq.top();   pq.pop();
-	        b = pq.top();   pq.pop();
-	        cost += a+b;
-	        if(pq.empty())  break;
-	        pq.push(a+b);
-	    }
-	    
-	    if(!pq.empty()) {
-	        cost += pq.top();
-	    }
-	    
-	    cout << cost << endl;
-	}
+        long long int a, b, cost = 0;
+        while(pq.size() > 1) {
+            a = pq.top();   pq.pop();
+            b = pq.top();   pq.pop();
+            cost += a+b;
+            if(pq.empty())  break;
+            pq.push(a+b);
+        }
+        
+        if(!pq.empty()) {
+            cost += pq.top();
+        }
+        
+        cout << cost << endl;
+    }
 }
