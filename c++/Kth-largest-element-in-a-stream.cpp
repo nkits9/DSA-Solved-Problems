38fe5ea14a997a2b548b832efe5b3f244cfd53e8
Ankit Sharma <nkits9@gmail.com>
2022-07-02 14:59:40 +0530

    Update Kth-largest-element-in-a-stream.cpp

--- c++/Kth-largest-element-in-a-stream.cpp
+++ c++/Kth-largest-element-in-a-stream.cpp
@@ -1,41 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-
 class cmp {
-	public:
-		bool operator()(int &A,int &B) {
-			return A > B;
-		}
+public:
+    bool operator()(int &A,int &B) {
+        return A > B;
+    }
 };
 
-int main() {
-	IOS;
-	int t; cin >> t;
-	while(t--) {
-        int k, n;
-        cin >> k >> n;
-        priority_queue<int, vector<int>, cmp> pq;
-        int x;
-        for(int i=0; i<n; i++) {
-            cin >> x;
-            if(i < k-1) {
-                pq.push(x);
-                cout << -1 << " ";
+vector<int> kthLargest(int k, int arr[], int n) {
+    vector<int> res;
+    priority_queue<int, vector<int>, cmp> pq;
+
+    for(int i=0; i<n; i++) {
+        if(i < k-1) {
+            pq.push(arr[i]);
+            res.push_back(-1);
+        }
+        else if(i == k-1) {
+            pq.push(arr[i]);
+            res.push_back(pq.top());
+        }
+        else {
+            if(arr[i] > pq.top()) {
+                pq.pop();
+                pq.push(arr[i]);
             }
-            else if(i == k-1) {
-                pq.push(x);
-                cout << pq.top() << " ";
-            }
-            else {
-                if(x > pq.top()) {
-                    pq.pop();
-                    pq.push(x);
-                }
-                cout << pq.top() << " ";
-            }
-    	}  
-        cout << endl;
-	}
+            res.push_back(pq.top());
+        }
+    }
+
+    return res;
 }
