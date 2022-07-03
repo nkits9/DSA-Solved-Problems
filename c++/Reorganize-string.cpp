17da8c9529b539c792a7c4e793dacfd00c4d47ce
Ankit Sharma <ankit27.sharma@paytm.com>
2022-07-03 11:49:38 +0530

    Update Reorganize String

---
+++ c++/Reorganize-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> mp;
+        for(char c: s) {
+            mp[c]++;
+        }
+        
+        priority_queue<pair<int, char>> pq;
+        for(auto it=mp.begin(); it!=mp.end(); it++) {
+            char c = it->first;
+            int freq = it->second;
+            pq.push({freq, c});
+        }
+        
+        string res = "";
+        
+        while(pq.size() > 1) {
+            pair<int, char> p1 = pq.top();
+            pq.pop();
+            pair<int, char> p2 = pq.top();
+            pq.pop();
+       
+            res += p1.second;
+            res += p2.second;
+            
+            p1.first--;
+            p2.first--;
+            
+            if(p1.first > 0) {
+                pq.push(p1);
+            }
+            if(p2.first > 0) {
+                pq.push(p2);
+            }
+        }
+        
+        if(!pq.empty()) {
+            if(pq.top().first > 1)     // curr_ele_freq > 1 and no other elements present
+                return "";
+            else
+                res += pq.top().second;
+        }
+        
+        return res;
+    }
+};
