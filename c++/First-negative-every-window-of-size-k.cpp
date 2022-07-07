b1400d976721ff94c229703c6b1800ba793c9e79
Ankit Sharma <nkits9@gmail.com>
2022-07-07 19:43:50 +0530

    Update First-negative-every-window-of-size-k.cpp

--- c++/First-negative-every-window-of-size-k.cpp
+++ c++/First-negative-every-window-of-size-k.cpp
@@ -1,29 +1,32 @@
 vector<long long> printFirstNegativeInteger(long long int arr[], long long int n, long long int k) {
-
     vector<long long> res;
-    
     queue<int> q;
     
-    for(int i=0; i<k-1; i++) {
-        if(arr[i] < 0) 
+    for(int i=0; i<n; i++) {
+        if(arr[i] < 0) {
             q.push(i);
-    }
-    
-    for(int i=k-1; i<n; i++) {
-        if(arr[i] < 0) 
-            q.push(i);
-        
-        if(!q.empty() && q.front() < i-k+1) {
-            q.pop();
         }
         
-        if(q.empty())
-            res.push_back(0);
-        else
-            res.push_back(arr[q.front()]);
-            
+        if(i < k-1) {
+            continue;
+        }
+        else if(i == k-1) {
+            if(q.empty())
+                res.push_back(0);
+            else
+                res.push_back(arr[q.front()]);
+        }
+        else {
+            if(!q.empty() && q.front() <= i-k) {
+                q.pop();
+            }
+            if(q.empty())
+                res.push_back(0);
+            else
+                res.push_back(arr[q.front()]);
+        }
+        
     }
-    
-    return res;
 
+    return res;
 }
