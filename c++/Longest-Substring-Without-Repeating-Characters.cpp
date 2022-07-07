1575fe1c1f0b9ee2eeaa5a0b93c6045b02bcf377
Ankit Sharma <nkits9@gmail.com>
2022-07-07 23:46:28 +0530

    Update Longest-Substring-Without-Repeating-Characters.cpp

--- c++/Longest-Substring-Without-Repeating-Characters.cpp
+++ c++/Longest-Substring-Without-Repeating-Characters.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     
     int lengthOfLongestSubstring(string s) {
-
-        int currLen=0, maxLen=0, n=s.size();
-
-        unordered_map<char, int> m;
-
-        for(int i=0; i<n; i++) {
-            currLen++;
-            
-            if(m.find(s[i]) != m.end()) {
-                currLen = min(currLen, i - m[s[i]]);
+        int minIdx = -1, ans = 0;
+        unordered_map<char, int> mp;
+        
+        for(int i=0; i<s.size(); i++) {
+            if(mp.find(s[i]) != mp.end()) {
+                minIdx = max(minIdx, mp[s[i]]);
             }
-            
-            m[s[i]] = i;
-            maxLen = max(maxLen, currLen);
+            mp[s[i]] = i;
+            ans = max(ans, i - minIdx);
         }
         
-        return maxLen;
+        return ans;
     }
+    
 };
