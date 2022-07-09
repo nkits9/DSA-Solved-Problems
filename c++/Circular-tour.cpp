363b2cb4634a1aba1a23de5a4f4c735be5afd328
Ankit Sharma <nkits9@gmail.com>
2022-07-09 16:49:40 +0530

    Update Circular-tour.cpp

--- c++/Circular-tour.cpp
+++ c++/Circular-tour.cpp
@@ -33,16 +33,22 @@ int tour(petrolPump p[], int n) {
 
 
 //Method 2 :
-int tour(petrolPump p[],int n) {
-    int start=0, curr=0, sum = 0;
-    for(int i=0; i<n; i++) {
-        curr += p[i].petrol - p[i].distance;
-        if(curr < 0) {
-            sum += curr;
-            curr = 0;
+
+int tour(petrolPump p[], int n) {
+    int currPetrol = 0, start = 0, i = 0;
+    int remPetrol = 0;      // Petrol needed at last
+
+    while(i<n) {
+        currPetrol += (p[i].petrol - p[i].distance);
+        if(currPetrol < 0) {
             start = i+1;
-        } 
-        if(i==n-1 && curr+sum<0)    return -1;
+            remPetrol += currPetrol;
+            currPetrol = 0;
+        }
+        i++;
     }
+
+    if(currPetrol + remPetrol < 0)  return -1;
+
     return start;
 }
