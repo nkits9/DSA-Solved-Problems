42197dfc9a53b926f19bb7b6fec1c718e395dae0
Ankit Sharma <nkits9@gmail.com>
2022-07-10 12:24:39 +0530

    Update 0-1-Knapsack-Problem.cpp

--- c++/0-1-Knapsack-Problem.cpp
+++ c++/0-1-Knapsack-Problem.cpp
@@ -1,45 +1,28 @@
 // Method-1 : Bottom Up Approach
-#include <bits/stdc++.h>
-using namespace std;
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-
-int main() {
-    IOS;
-    // Code starts from here:
-    int t; cin>>t;
-    while(t--) {
-        int n; cin>>n;
-        int W; cin>>W;
-        int val[n], wt[n];
-        for(int i=0;i<n;i++)
-            cin>>val[i];
-        for(int i=0;i<n;i++)
-            cin>>wt[i];
+int knapSack(int W, int wt[], int val[], int n) { 
+    int dp[n+1][W+1];
+    
+    for(int i=0; i<=n; i++) {
         
-        int dp[n+1][W+1];
-
-        
-        for(int i=0; i<=n; i++) {
+        for(int j=0; j<=W; j++) {
             
-            for(int j=0; j<=W; j++) {
-                if(i==0||j==0) {
-                    dp[i][j]=0;
-                }
-                else if(wt[i-1]<=j) {
-                    dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
-                }
-                else {
-                    dp[i][j]=dp[i-1][j];
-                }
+            if(i==0||j==0) {
+                dp[i][j]=0;
             }
+            else if(wt[i-1]<=j) {
+                dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+            }
+            else {
+                dp[i][j]=dp[i-1][j];
+            }
+            
         }
-        cout<<dp[n][W]<<endl;
-       // memset(dp,0,sizeof(dp));
-        
     }
-    return 0;
+    
+    return dp[n][W];
 }
+
+
 
 /*
 -------------------------------------------------------------------------------------------------------------------------------
