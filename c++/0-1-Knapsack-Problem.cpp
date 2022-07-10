69301345431176f37094e387cb8368d6333ae04e
Ankit Sharma <nkits9@gmail.com>
2022-07-10 12:14:20 +0530

    Update 0-1-Knapsack-Problem.cpp

--- c++/0-1-Knapsack-Problem.cpp
+++ c++/0-1-Knapsack-Problem.cpp
@@ -49,22 +49,20 @@ int main() {
 
 //Function to return max value that can be put in knapsack of capacity W.
 int solve(int W, int wt[], int val[], int n, vector<vector<int>> &dp) {
-    if(n==0) {
-        return 0;
-    }
+    if(n==0)    return 0;
 
-    if(dp[n-1][W] != -1)    return dp[n-1][W];
+    if(dp[n][W] != -1)    return dp[n][W];
 
     if(W >= wt[n-1]) 
-        dp[n-1][W] = max(val[n-1] + solve(W-wt[n-1], wt, val, n-1, dp), solve(W, wt, val, n-1, dp));
+        dp[n][W] = max(val[n-1] + solve(W-wt[n-1], wt, val, n-1, dp), solve(W, wt, val, n-1, dp));
     else 
-        dp[n-1][W] = solve(W, wt, val, n-1, dp);
+        dp[n][W] = solve(W, wt, val, n-1, dp);
 
-    return dp[n-1][W];
+    return dp[n][W];
 }
 
 
 int knapSack(int W, int wt[], int val[], int n) { 
-    vector<vector<int>> dp(n, vector<int>(W+1, -1));
+    vector<vector<int>> dp(n+1, vector<int>(W+1, -1));
     return solve(W, wt, val, n, dp);
 }
