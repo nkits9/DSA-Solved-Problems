b034aed6b2d3d6d983eaf8bec799e44062008810
Ankit Sharma <nkits9@gmail.com>
2022-07-16 14:06:02 +0530

    Update Implement-Queue-using-Linked-List.cpp

--- c++/Implement-Queue-using-Linked-List.cpp
+++ c++/Implement-Queue-using-Linked-List.cpp
@@ -1,46 +1,49 @@
 //Structure of a node in Queue
-struct QueueNode
-{
+struct QueueNode {
     int data;
     QueueNode *next;
-    QueueNode(int a)
-    {
+    QueueNode(int a) {
         data = a;
         next = NULL;
     }
 };
 
-//Structure of MyQueue
-struct MyQueue {
+
+class MyQueue {
+    private:
     QueueNode *front;
     QueueNode *rear;
-    void push(int);
-    int pop();
-    MyQueue() {front = rear = NULL;}
-};
-
-void MyQueue:: push(int x) {
-    if(rear==NULL) {
-        rear = new QueueNode(x);
-        front = rear;
-    } else {
-        rear->next = new QueueNode(x);
-        rear = rear->next;
-    }
-}
-
-int MyQueue :: pop() {
-    if(front == NULL)   return -1;
-    int val;
-    if(front==rear) {
-        val = front->data;
-        delete(front);
+    
+    public:
+    MyQueue() {
         front = rear = NULL;
-    } else {
-        QueueNode* temp = front->next;
-        val = front->data;
-        delete(front);
-        front = temp;
     }
-    return val;
-}
+    
+    public:
+    void push(int x) {
+        if(rear==NULL) {
+            rear = new QueueNode(x);
+            front = rear;
+        } else {
+            rear->next = new QueueNode(x);
+            rear = rear->next;
+        }
+    }
+    
+    public:
+    int pop() {
+        if(front == NULL)   return -1;
+        int val;
+        if(front==rear) {
+            val = front->data;
+            delete(front);
+            front = rear = NULL;
+        } else {
+            QueueNode* temp = front->next;
+            val = front->data;
+            delete(front);
+            front = temp;
+        }
+        return val;
+    }
+};
