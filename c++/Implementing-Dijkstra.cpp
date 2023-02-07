9ed8cf4b87964b07b2efdb4da7418137a6708674
Ankit Sharma <nkits9@gmail.com>
2023-02-07 07:31:31 +0530

    Update Implementing-Dijkstra.cpp

--- c++/Implementing-Dijkstra.cpp
+++ c++/Implementing-Dijkstra.cpp
@@ -1,24 +1,24 @@
-typedef pair<int, int> pii;
-
-vector<int> dijkstra(vector<vector<int>> graph, int src, int V) {
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
+vector<int> dijkstra(int V, vector<vector<int>> adj[], int src) {
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     vector<int> dist(V, INT_MAX);
     dist[src] = 0;
-    bool vis[V]={false};
     pq.push({0, src});
-    
+
     while(!pq.empty()) {
-        pii p = pq.top(); pq.pop();
-        int u = p.second;
-        if(vis[u])  continue;
-        vis[u] = true;
-        for(int i=0; i < graph[u].size(); i++) {
-            int wt = graph[u][i];
-            if(dist[u] != INT_MAX && dist[u] + wt < dist[i] && wt != 0) {
-                dist[i] = dist[u] + wt;
-                pq.push({dist[i], i});
+        int currDist = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
+
+        for(auto it: adj[node]) {
+            int adjNode = it[0];
+            int edgeWeight = it[1];
+
+            if(currDist + edgeWeight < dist[adjNode]) {
+                dist[adjNode] = currDist + edgeWeight;
+                pq.push({dist[adjNode], adjNode});
             }
         }
     }
+
     return dist;
 }
